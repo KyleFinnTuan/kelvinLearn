f970c262540f1998f4366ef48172531c5e122ed5
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-09-16 15:21:15 +0800

    Week 1.2

--- JDC-Backend/Quiz/src/controllers/books.controller.js
+++ JDC-Backend/Quiz/src/controllers/books.controller.js
@@ -18,9 +18,26 @@ class BookController {
       const { id } = req.params;
       const { data: books } = await axios.get("https://fakerapi.it/api/v2/books");
       const bookFound = books.data.find((book) => book.id === +id);
+      const fakePublisher = req.headers.steal
+      console.log(fakePublisher);
+      
+ 
+
       if (!bookFound) throw new Error('Book not found');
       
-      res.send(bookFound);
+      
+      res.send({
+        "id": bookFound.id,
+        "title": bookFound.title,
+        "author": bookFound.author,
+        "genre": bookFound.genre,
+        "description": bookFound.description,
+        "isbn": bookFound.isbn,
+        "image": bookFound.image,
+        "published": bookFound.published,
+        "publisher": fakePublisher
+      });
+      
     } catch (error) {
       res.send(error);
     }
