180c8148efec120897fae7929480315f4183cc86
KyleFinnTuan <wandinainggolan9@gmail.com>
2024-10-01 23:16:13 +0800

    quiz txt

--- JDC-Backend/Quiz/src/controllers/books.controller.js
+++ JDC-Backend/Quiz/src/controllers/books.controller.js
@@ -1,45 +1,41 @@
 // import {} from "../Utils/consts.js";
-import axios from 'axios';
+import axios from "axios";
 
 class BookController {
-    getBook = async (req, res) => {
-        try {
-          const { limit } = req.query;
-          const response = await axios.get(`https://fakerapi.it/api/v2/books?_quantity=${limit}`);
-    
-          res.send(response.data.data);
-        } catch (error) {
-          res.send(error);
-        }
-      };
+  getBook = async (req, res) => {
+    try {
+      const { limit } = req.query;
+      const response = await axios.get(`https://fakerapi.it/api/v2/books?_quantity=${limit}`);
+      res.send(response.data.data);
+    } catch (error) {
+      res.send(error);
+    }
+  };
 
   findBook = async (req, res) => {
     try {
       const { id } = req.params;
       const { data: books } = await axios.get("https://fakerapi.it/api/v2/books");
       const bookFound = books.data.find((book) => book.id === +id);
-      const fakePublisher = req.headers.steal
-      console.log(fakePublisher);
-      
- 
+      const fakePublisher = req.headers.steal;
+      console.log(bookFound);
 
-      if (!bookFound) throw new Error('Book not found');
-      
-      
-      res.send({
-        "id": bookFound.id,
-        "title": bookFound.title,
-        "author": bookFound.author,
-        "genre": bookFound.genre,
-        "description": bookFound.description,
-        "isbn": bookFound.isbn,
-        "image": bookFound.image,
-        "published": bookFound.published,
-        "publisher": fakePublisher
+      if (!bookFound) throw { message: 'book not found', error: new Error("findBook Error") }
+
+      res.status(200).send({
+        id: bookFound.id,
+        title: bookFound.title,
+        author: bookFound.author,
+        genre: bookFound.genre,
+        description: bookFound.description,
+        isbn: bookFound.isbn,
+        image: bookFound.image,
+        published: bookFound.published,
+        publisher: fakePublisher,
       });
-      
     } catch (error) {
-      res.send(error);
+      console.log(error.error);
+      res.status(500).send(error);
     }
   };
 }
